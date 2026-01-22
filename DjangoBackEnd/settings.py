d9a3effea50ba6b43afa62db11d67568668d9523
ColleFALL <collefall118@gmail.com>
2026-01-22 11:37:54 +0000

    cloudnary1

--- DjangoBackEnd/settings.py
+++ DjangoBackEnd/settings.py
@@ -58,7 +58,7 @@ INSTALLED_APPS = [
     
 ]
 
-DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+# DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 
 MIDDLEWARE = [
@@ -142,12 +142,15 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+# ✅ Required: évite l'erreur settings.STATICFILES_STORAGE
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 CLOUDINARY_STORAGE = {
     "CLOUD_NAME": os.environ.get("CLOUDINARY_CLOUD_NAME"),
     "API_KEY": os.environ.get("CLOUDINARY_API_KEY"),
     "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET"),
 }
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 
 REST_FRAMEWORK = {
