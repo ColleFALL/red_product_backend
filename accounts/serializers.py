d97d15d0a929ae3ff943d69311baf568b334b73e
ColleFALL <collefall118@gmail.com>
2026-01-29 09:30:07 +0000

    corrections des fichiers

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,20 +1,49 @@
+# from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+# from djoser.serializers import UserSerializer as BaseUserSerializer
+# from django.contrib.auth import get_user_model
+
+# User = get_user_model()
+
+# class UserCreateSerializer(BaseUserCreateSerializer):
+#     class Meta(BaseUserCreateSerializer.Meta):
+#         model = User
+#         fields = ("id", "email", "name", "password")
+#         extra_kwargs = {"password": {"write_only": True}, "name": {"required": False}}
+
+#     def create(self, validated_data):
+#         # username généré automatiquement dans UserManager
+#         return User.objects.create_user(**validated_data)
+
+# class UserSerializer(BaseUserSerializer):
+#     class Meta(BaseUserSerializer.Meta):
+#         model = User
+#         fields = ("id", "email", "name", "username", "is_active", "date_joined")
+#         read_only_fields = ("id", "username", "is_active", "date_joined")
+
 from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
 from djoser.serializers import UserSerializer as BaseUserSerializer
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class UserCreateSerializer(BaseUserCreateSerializer):
+    """Serializer pour la création d'utilisateur avec Djoser"""
     class Meta(BaseUserCreateSerializer.Meta):
         model = User
         fields = ("id", "email", "name", "password")
-        extra_kwargs = {"password": {"write_only": True}, "name": {"required": False}}
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "name": {"required": False}
+        }
 
     def create(self, validated_data):
-        # username généré automatiquement dans UserManager
+        # Le username sera généré automatiquement dans UserManager
         return User.objects.create_user(**validated_data)
 
+
 class UserSerializer(BaseUserSerializer):
+    """Serializer pour afficher les informations utilisateur"""
     class Meta(BaseUserSerializer.Meta):
         model = User
         fields = ("id", "email", "name", "username", "is_active", "date_joined")
