aaeedad0aeb6db93c0f73eef72904f944851aa48
ColleFALL <collefall118@gmail.com>
2026-02-17 20:46:31 +0000

    mis a jour avant chat bot

--- accounts/models.py
+++ accounts/models.py
@@ -1,40 +1,4 @@
-# from django.db import models
-# from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
-# from django.utils import timezone
 
-# class UserManager(BaseUserManager):
-#     def create_user(self, email, password=None, **extra_fields):
-#         if not email:
-#             raise ValueError("L'adresse email est obligatoire")
-#         email = self.normalize_email(email)
-#         if not extra_fields.get("username"):
-#             extra_fields["username"] = email.split("@")[0]
-#         user = self.model(email=email, **extra_fields)
-#         user.set_password(password)
-#         user.save(using=self._db)
-#         return user
-
-#     def create_superuser(self, email, password=None, **extra_fields):
-#         extra_fields.setdefault("is_staff", True)
-#         extra_fields.setdefault("is_superuser", True)
-#         extra_fields.setdefault("is_active", True)
-#         return self.create_user(email, password, **extra_fields)
-
-# class User(AbstractBaseUser, PermissionsMixin):
-#     username = models.CharField(max_length=150, default='user_default')
-#     email = models.EmailField(unique=True)
-#     name = models.CharField(max_length=255, blank=True)
-#     is_active = models.BooleanField(default=True)
-#     is_staff = models.BooleanField(default=False)
-#     date_joined = models.DateTimeField(default=timezone.now)
-
-#     objects = UserManager()
-
-#     USERNAME_FIELD = "email"
-#     REQUIRED_FIELDS = []
-
-#     def __str__(self):
-#         return self.email
 
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
